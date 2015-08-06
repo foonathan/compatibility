380c029f19e0b9f2175d93fa1ffb8e01eeab6bde
Jonathan Müller <jonathanmueller.dev@gmail.com>
2015-08-06 17:57:34 +0200

    added tests and workarounds for some missing C++11 library features

---
+++ cpp11_lib.cmake
@@ -0,0 +1,7 @@
+# Copyright (C) 2015 Jonathan Müller <jonathanmueller.dev@gmail.com>
+# This file is subject to the license terms in the LICENSE file
+# found in the top-level directory of this distribution.
+
+include(cpp11_lib/get_new_handler.cmake)
+include(cpp11_lib/get_terminate.cmake)
+include(cpp11_lib/max_align_t.cmake)
