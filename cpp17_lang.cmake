b4cb3595f5e3841b9c56d75883ef081c595ef4bb
Jonathan Müller <jonathanmueller.dev@gmail.com>
2015-11-03 13:31:48 +0100

    Add accepted C++17 language features
    Adds all accepted C++17 language features that aren't syntax sugar
    (typename instead of class, auto, namespace,...).

---
+++ cpp17_lang.cmake
@@ -0,0 +1,7 @@
+# Copyright (C) 2015 Jonathan Müller <jonathanmueller.dev@gmail.com>
+# This file is subject to the license terms in the LICENSE file
+# found in the top-level directory of this distribution.
+
+include(cpp17_lang/fold_expressions.cmake)
+include(cpp17_lang/terse_static_assert.cmake)
+include(cpp17_lang/utf8_char_literal.cmake)
