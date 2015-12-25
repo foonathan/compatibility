b6ae603f3934289ace5204e570e17f574f08faae
Jonathan Müller <jonathanmueller.dev@gmail.com>
2015-12-25 22:37:39 +0100

    Add cpp14_lib/quoted test

---
+++ cpp14_lib/quoted.cmake
@@ -0,0 +1,19 @@
+# Copyright (C) 2015 Jonathan Müller <jonathanmueller.dev@gmail.com>
+# This file is subject to the license terms in the LICENSE file
+# found in the top-level directory of this distribution.
+
+if(NOT COMP_API_VERSION)
+    message(FATAL_ERROR "needs newer comp_base.cmake version")
+endif()
+comp_api_version(1)
+
+comp_feature(quoted "#include <iomanip>
+                     #include <sstream>
+
+                     int main()
+                     {
+                        std::stringstream ss;
+                        std::string foo = "a b";
+                        ss << std::quoted(foo);
+                        ss >> std::quoted(foo);
+                     }" COMP_CPP14_FLAG)
