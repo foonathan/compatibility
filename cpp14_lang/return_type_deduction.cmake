5d14d6cc0bdbd9ccee0558fbb1855c9115bc1883
Jonathan Müller <jonathanmueller.dev@gmail.com>
2015-12-27 17:42:48 +0100

    Add cpp14_lang/return_type_deduction test and workaround

---
+++ cpp14_lang/return_type_deduction.cmake
@@ -0,0 +1,21 @@
+# Copyright (C) 2015 Jonathan Müller <jonathanmueller.dev@gmail.com>
+# This file is subject to the license terms in the LICENSE file
+# found in the top-level directory of this distribution.
+
+if(NOT COMP_API_VERSION)
+    message(FATAL_ERROR "needs newer comp_base.cmake version")
+endif()
+comp_api_version(1)
+
+comp_feature(return_type_deduction
+        "auto foo()
+        {
+            return 2;
+        }
+
+        int main() {}" COMP_CPP14_FLAG)
+
+comp_workaround(return_type_deduction
+"#ifndef ${COMP_PREFIX}AUTO_RETURN
+    #define ${COMP_PREFIX}AUTO_RETURN(...) decltype(__VA_ARGS__) {return (__VA_ARGS__);}
+#endif" COMP_CPP11_FLAG)
