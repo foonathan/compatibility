76659a70ec4793af7fa4c4d1734f1462e4a7d9b5
Kevin Brightwell <kevin.brightwell2@gmail.com>
2016-08-15 21:16:11 -0400

    Add `cxx_strong_enums` support

---
+++ cpp11_lang/strong_enum.cmake
@@ -0,0 +1,11 @@
+# Copyright (C) 2015-2016 Jonathan Müller <jonathanmueller.dev@gmail.com>
+# This file is subject to the license terms in the LICENSE file
+# found in the top-level directory of this distribution.
+
+if(NOT COMP_API_VERSION)
+    message(FATAL_ERROR "needs newer comp_base.cmake version")
+endif()
+comp_api_version(1)
+
+comp_feature(auto_type "enum class Colour: unsigned int { Red = 1, Blue = 3 };
+int main() { const Colour c = Colour::Red; }" COMP_CPP11_FLAG)
