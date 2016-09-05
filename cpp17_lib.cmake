45a88df4e798e0473204ea63e13f9e4e7e71e486
muued <muued@users.noreply.github.com>
2016-09-05 10:10:26 +0200

    inclue -> include

--- cpp17_lib.cmake
+++ cpp17_lib.cmake
@@ -3,9 +3,9 @@
 # found in the top-level directory of this distribution.
 
 comp_fetch_include(cpp17_lib/bool_constant)
-comp_fetch_inclue(cpp17_lib/container_access)
-comp_fetch_inclue(cpp17_lib/invoke)
-comp_fetch_inclue(cpp17_lib/map_insertion)
-comp_fetch_inclue(cpp17_lib/shared_mutex)
-comp_fetch_inclue(cpp17_lib/uncaught_exceptions)
-comp_fetch_inclue(cpp17_lib/void_t)
+comp_fetch_include(cpp17_lib/container_access)
+comp_fetch_include(cpp17_lib/invoke)
+comp_fetch_include(cpp17_lib/map_insertion)
+comp_fetch_include(cpp17_lib/shared_mutex)
+comp_fetch_include(cpp17_lib/uncaught_exceptions)
+comp_fetch_include(cpp17_lib/void_t)
